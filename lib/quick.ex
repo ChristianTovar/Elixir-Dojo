d280a10922c44fd4a4005305ad1b8e4c5d13dd67
ChristianTovar <tovarchristian21@gmail.com>
2020-02-10 17:00:32 -0500

    renamed variable

--- lib/quick.ex
+++ lib/quick.ex
@@ -1,6 +1,7 @@
 defmodule Quick do
-  def sort([a]) ,do: [a]
-  def sort([]) ,do: []
+  def sort([value]), do: [value]
+  def sort([]), do: []
+
   def sort([pivot | tail]) do
     {left, right} = Enum.split_with(tail, fn value -> value < pivot end)
     sort(left) ++ [pivot] ++ sort(right)
